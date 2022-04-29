4d15756bd6f40c5caecbbe54c324fb8ecd4a3fdf
Konstantin <example@gmail.com>
2022-04-29 11:00:15 +0400

    third redact

--- js/script.js
+++ js/script.js
@@ -1,1 +1,28 @@
-console.log(1);
+let numberOfFilms = +prompt('Сколько фильмов Вы уже посмотрели?', '');
+
+let lastFilm = prompt('Один из последних просмотренных фильмов','');
+
+let rate = +prompt('На сколько оцените его?', '');
+
+let lastFilm2 = prompt('Один из последних просмотренных фильмов','');
+
+let rate2 = +prompt('На сколько оцените его?', '');
+
+// console.log(result);
+
+let personalMovieDB = {
+    count: `${numberOfFilms}`,
+    movies: {
+        // [lastFilm]:`${rate}`,
+        // [lastFilm2]:`${rate2}`
+    },
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+personalMovieDB.movies[lastFilm] = rate;
+personalMovieDB.movies[lastFilm2] = rate2;
+
+
+console.log(personalMovieDB);
